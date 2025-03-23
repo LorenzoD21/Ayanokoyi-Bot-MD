59879d8168a63848d2d9b7cce3c221ce11502b57
LorenzoD21 <lorenzobuten1@gmail.com>
2025-03-23 03:13:52 -0400

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -35,7 +35,7 @@ else return m.reply(`*[ ⚠️ ] El url es incorrecto*`)
 console.error(e)
 if (!stiker) stiker = e
 } finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: 'Տһ͟ᥲ֟፝ძ᥆ϣ  Ϟ  S𝗍іᥴkᥱrㅤ☕', body: `Shadow Bot MD`, mediaType: 2, sourceUrl: grupo, thumbnail: icons}}}, { quoted: m })
+if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: 'ayanokoyi Ϟ  S𝗍іᥴkᥱrㅤ☕', body: `kiyotaka Bot MD`, mediaType: 2, sourceUrl: grupo, thumbnail: icons}}}, { quoted: m })
 
 else return conn.reply(m.chat, `*[ ℹ️ ] La conversión ah fallado, responde a un vídeo, imagen o gif lo cual será convertido en sticker.*`, m, rcanal)
 
